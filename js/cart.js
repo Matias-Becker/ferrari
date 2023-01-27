bccfe13d115031bc60bb8cb58bb3a4f6900bf5e2
Matias Becker <matiasbecker2711@gmail.com>
2023-01-26 22:01:31 -0300

    Tercera preentrega curso js

---
+++ js/cart.js
@@ -0,0 +1,101 @@
+let carrito=[]
+
+const productoContenedor= document.getElementById("producto-contenedor") //revisar
+
+productoContenedor.addEventListener("click", (e)=>{
+    if(e.target.classlist.contains("arti__boton")){             //revisar
+        validarProductoRepetido(e.target.id)
+    }
+    console.log();
+})
+
+const validarProductoRepetido=(productoId)=>{
+    const productoRepetido= carrito.find(producto=> producto.id==productoId)
+
+    if(localStorage.getItem("carrito")){
+        carrito=obtenerCarritoStorage()
+    }
+
+    if(!productoRepetido){
+        const producto=productos.find(producto=> producto.id==productoId)
+        carrito.push(producto)
+        pintarProductoCarrito(producto)
+        actualizarTotalesCarrito(carrito)
+    }else{
+        productoRepetido.cantidad++
+        const cantidadProducto= document.getElementById(`cantidad${productoRepetido.id}`)
+        cantidadProducto.innerText= `Cantidad: ${productoRepetido.cantidad}`
+        actualizarTotalesCarrito(carrito)
+    }
+}
+
+const pintarProductoCarrito= (producto)=>{
+    const contenedor= document.getElementById("carrito-contenedor") //hacer contenedor en el html
+    const div= document.createElement("div")
+    div.classList.add("productoEnCarrito")  //hacer esta clase en el css
+    div.innerHTML=`
+        <p>${producto.nombre}</p>
+        <p>Precio: ${producto.precio}</p>
+        <p id=cantidad${producto.id}>Cantidad: ${producto.cantidad}</p>
+        <button class="btn waves-effect waves-light boton-eliminar" value="${producto.id}">X</button>
+    `
+    contenedor.appendChild(div)
+}
+
+const actualizarTotalesCarrito= (carrito)=>{
+    const totalCantidad= carrito.reduce((acc, item)=> acc+item.cantidad, 0)
+    const totalCompra= carrito.reduce((acc, item)=> acc+(item.precio*item.cantidad), 0)
+    
+    pintarTotalesCarrito(totalCantidad, totalCompra)
+    guardarCarritoStorage(carrito)
+}
+
+const pintarTotalesCarrito= (totalCantidad, totalCompra)=>{
+    const contadorCarrito= document.getElementById("contador-carrito") //revisar parametro
+    const precioTotal= document.getElementById("precioTotal")  //revisar parametro
+    contadorCarrito.innerText= totalCantidad
+    precioTotal.innerText= totalCompra
+}
+
+const pintarCarrito= (carrito)=>{
+    const contenedor= document.getElementById("carrito-contenedor") //hacer contenedor en el html
+
+    contenedor.innerHTML=""
+    
+    carrito.forEach(producto => {
+        const div= document.createElement("div")
+        div.classList.add("productoEnCarrito")  //hacer esta clase en el css
+        div.innerHTML=`
+            <p>${producto.nombre}</p>
+            <p>Precio: ${producto.precio}</p>
+            <p id=cantidad${producto.id}>Cantidad: ${producto.cantidad}</p>
+            <button class="btn waves-effect waves-light boton-eliminar" value="${producto.id}">X</button>
+        `
+        contenedor.appendChild(div)
+    });
+    
+}
+
+const eliminarProductoCarrito= (productoId)=>{
+    const productoIndex= carrito.findIndex(producto=> producto.id==productoId)
+    carrito.splice(productoIndex, 1)
+    pintarCarrito(carrito)
+    actualizarTotalesCarrito(carrito)
+}
+
+const guardarCarritoStorage= (carrito)=>{
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+}
+
+const obtenerCarritoStorage= ()=>{
+    const carritoStorage= JSON.parse(localStorage.getItem("carrito"))
+    return carritoStorage
+}
+
+const cargarCarrito= ()=>{
+    if(localStorage.getItem("carrito")){
+        carrito= obtenerCarritoStorage()
+        pintarCarrito(carrito)
+        actualizarTotalesCarrito(carrito)
+    }
+}
